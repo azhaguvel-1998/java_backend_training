2ce23f0ef1a5303f66008b233a64b249b2c5739a
azhaguvel.manikkam <azhaguvel.manikkam@collabera.com>
2022-01-04 13:53:09 +0530

    Additiont

---
+++ javasimpleprogram/src/test/java/AdditionTesting.java
@@ -0,0 +1,15 @@
+public class AdditionTesting {
+    public void testVariables(){
+        int age=10;
+        float sal=20.6f;
+        char ch="a";
+        boolean isManager=false;
+        byte byt=10;
+        short sh=10;
+        long lng=46464646L;
+        String nm="android";
+    }
+    public void testMethods(){
+
+    }
+}
