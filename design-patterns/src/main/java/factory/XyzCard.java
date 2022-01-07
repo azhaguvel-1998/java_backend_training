ebecf3053c85d189fa6edfa800e6456104cca97d
azhaguvel.manikkam <azhaguvel.manikkam@collabera.com>
2022-01-07 13:24:24 +0530

    design patterns : factory

---
+++ design-patterns/src/main/java/factory/XyzCard.java
@@ -0,0 +1,14 @@
+package factory;
+
+public class XyzCard implements CardProvider {
+    @Override
+    public void swipe(float amt) {
+
+    }
+
+    @Override
+    public String getBank() {
+        return "xyz";
+    }
+
+}
