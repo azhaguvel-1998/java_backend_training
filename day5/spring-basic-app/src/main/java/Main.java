8c5b0b78944e43138f1b051372faec4e49a7856e
azhaguvel.manikkam <azhaguvel.manikkam@collabera.com>
2022-01-07 16:41:55 +0530

    spring:dependency injection intro

---
+++ day5/spring-basic-app/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import Car.Car;
+import Car.Driver;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car car = new Car();
+
+        Driver driver = new Driver();
+        driver.setCar(car);
+    }
+}
