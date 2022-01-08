82b625326c072dc3683454a04e5377d034dd0335
azhaguvel.manikkam <azhaguvel.manikkam@collabera.com>
2022-01-08 15:50:28 +0530

    spring:dependency injection intro

---
+++ day5/spring-basic-app2/src/main/java/Car/Car.java
@@ -0,0 +1,14 @@
+package Car;
+
+public class Car {
+    private int speed;
+
+    public int getSpeed() {
+
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
