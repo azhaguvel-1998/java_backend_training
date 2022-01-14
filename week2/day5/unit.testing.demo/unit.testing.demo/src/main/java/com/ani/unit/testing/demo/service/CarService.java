509e45aeb6d027162d45e620f74c5b10cbf2702c
azhaguvel.manikkam <azhaguvel.manikkam@collabera.com>
2022-01-14 12:28:22 +0530

    unit testing : service layer - tests

--- week2/day5/unit.testing.demo/unit.testing.demo/src/main/java/com/ani/unit/testing/demo/service/CarService.java
+++ week2/day5/unit.testing.demo/unit.testing.demo/src/main/java/com/ani/unit/testing/demo/service/CarService.java
@@ -2,6 +2,10 @@ package com.ani.unit.testing.demo.service;
 
 import com.ani.unit.testing.demo.domain.Car;
 
+import java.util.Optional;
+
 public interface CarService {
     Car saveCar(Car car);
+
+    Optional<Car> findCarById(Long id);
 }
--- week2/day5/unit.testing.demo/unit.testing.demo/src/main/java/com/ani/unit/testing/demo/service/CarServiceImpl.java
+++ week2/day5/unit.testing.demo/unit.testing.demo/src/main/java/com/ani/unit/testing/demo/service/CarServiceImpl.java
@@ -1,16 +1,21 @@
 package com.ani.unit.testing.demo.service;
 
 import com.ani.unit.testing.demo.domain.Car;
+import exception.InvalidIdException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-
-
 public class CarServiceImpl implements CarService {
     @Override
     public Car saveCar(Car car) {
 
         return car;
     }
-
+    @Override
+    public Optional<Car> findCarById(Long id) {
+        if(id <= 0) throw new InvalidIdException("Id must be positive : "+id);
+        return Optional.of(new Car());
+    }
 }
