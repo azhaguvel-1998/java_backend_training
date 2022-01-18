58fb2d47a30832b19c3a47634e80b52a5a3c254b
azhaguvel.manikkam <azhaguvel.manikkam@collabera.com>
2022-01-18 09:46:37 +0530

    microservice : inter service communication

---
+++ week3/microservices/abc-microservices/abc-microservices/src/main/java/com/ani/abc/AbcMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.ani.abc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcMicroservicesApplication.class, args);
+	}
+
+}
