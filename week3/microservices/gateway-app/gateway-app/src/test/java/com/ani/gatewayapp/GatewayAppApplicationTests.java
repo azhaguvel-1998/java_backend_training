0d5465929e01950be868c3e3a532db9552117b69
azhaguvel.manikkam <azhaguvel.manikkam@collabera.com>
2022-01-18 09:38:41 +0530

    microservice : inter service communication

---
+++ week3/microservices/gateway-app/gateway-app/src/test/java/com/ani/gatewayapp/GatewayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.gatewayapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
