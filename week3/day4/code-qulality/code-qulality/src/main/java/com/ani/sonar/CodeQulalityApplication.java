28b5c249d668408d539a42abaf6252ee95cd4366
azhaguvel.manikkam <azhaguvel.manikkam@collabera.com>
2022-01-20 12:40:31 +0530

    initial commit

---
+++ week3/day4/code-qulality/code-qulality/src/main/java/com/ani/sonar/CodeQulalityApplication.java
@@ -0,0 +1,13 @@
+package com.ani.sonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+ public class CodeQulalityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeQulalityApplication.class, args);
+	}
+
+}
---
+++ week3/day4/code-qulality/code-qulality/src/main/java/com/ani/sonar/domain/Msg.java
@@ -0,0 +1,13 @@
+package com.ani.sonar.domain;
+
+public class Msg {
+    private String mobile;
+
+     String getMobile() {
+        return mobile;
+    }
+
+     void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+}
